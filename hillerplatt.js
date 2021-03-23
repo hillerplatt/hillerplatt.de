82e546f2ba5df5b2d0a9d137a67ddcf018b84cc1
Jan C. Borchardt <hey@jancborchardt.net>
2021-03-23 15:36:26 +0100

    Update von Bootstrap und jQuery
    Signed-off-by: Jan C. Borchardt <hey@jancborchardt.net>

--- hillerplatt.js
+++ hillerplatt.js
@@ -1,25 +1,26 @@
 wortliste.forEach(function(wort) {
 	let plattdeutschId = wort.plattdeutsch.replace(/[^a-zA-Z ]/g, '')
 	let wortblock = document.createElement('li')
-	wortblock.classList.add('col-md-4', 'media')
+	wortblock.classList.add('col-md-4')
 	wortblock.setAttribute('id', plattdeutschId);
 
-	let block = ''
+	let block = '<div class="d-inline-flex p-2">'
 
 	// TEMPORÄR: Audio-Abspielfunktion erst für Wortblöcke, die bereits aufgenommen sind. Aktuell alle Worte mit a
 	if(wort.plattdeutsch.toLowerCase().startsWith('a') || wort.plattdeutsch.toLowerCase().startsWith('ä')) {
 		if(wort.artikel) wort.plattdeutsch = wort.artikel + ' ' + wort.plattdeutsch
 		block += '<audio id="' + plattdeutschId + '-audio" src="audio/recorder/hw-' + wort.plattdeutsch + '.flac" preload="none"></audio>'
-		block += '<button onclick="document.getElementById(\'' + plattdeutschId + '-audio\').play();" type="button" class="btn btn-primary mr-3 btn-play" aria-label="Abspielen">'
+		block += '<button onclick="document.getElementById(\'' + plattdeutschId + '-audio\').play();" type="button" class="btn btn-primary btn-play me-3" aria-label="Abspielen">'
 		block += '</button>'
 	} else {
 		if(wort.artikel) wort.plattdeutsch = wort.artikel + ' ' + wort.plattdeutsch
-		block += '<div class="mr-3"></div>'
+		block += '<div class="btn-placeholder me-3"></div>'
 	}
 
-	block += '<div class="media-body">'
-	block += '<h3 class="mt-0 mb-1">' + wort.plattdeutsch + '</h3>'
+	block += '<div>'
+	block += '<h3 class="mb-0">' + wort.plattdeutsch + '</h3>'
 	block += '<p>' + wort.hochdeutsch + '</p>'
+	block += '</div>'
 	block += '</div>'
 
 	wortblock.innerHTML = block
